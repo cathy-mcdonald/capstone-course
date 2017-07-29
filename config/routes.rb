653d88764c8e4a8824e04d39950bdb443b00d804
Cathy McDonald <github.cathy_mcdonald@spamgourmet.com>
2017-07-29 15:19:49 +1000

    Remove Foos

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   scope :api, defaults: {format: :json}  do 
     resources :cities, only: [:index, :show]
     resources :states, only: [:index, :show]
-    resources :foos, except: [:new, :edit]
-    resources :bars, except: [:new, :edit]
+      resources :bars, except: [:new, :edit]
   end
   
   get '/ui' => 'ui#index'
